e284df3904526793af39dd2869747e204c37b390
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-01-13 19:21:16 +0900

    カメラの使用方法

---
+++ Sample9-Camera/Camera.swift
@@ -0,0 +1,129 @@
+//
+//  Camera.swift
+//  Sample9-Camera
+//
+//  Created by keiji yamaki on 2021/01/13.
+//
+/*
+    カメラの使用方法
+ @ObservedObject var camera = Camera()              // カメラ情報の初期設定
+ CALayerView(caLayer: camera.previewLayer)          // カメラビューの表示
+ camera.takePhoto()                                 // 撮影
+ camera.image                                       // 撮影した画像
+ */
+import SwiftUI
+import Combine
+import AVFoundation
+
+class Camera: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, ObservableObject {
+    ///撮影した画像
+    @Published var image: UIImage?
+    ///プレビュー用レイヤー
+    var previewLayer:CALayer!
+    ///撮影開始フラグ
+    private var _takePhoto:Bool = false
+    ///セッション
+    private let captureSession = AVCaptureSession()
+    ///撮影デバイス
+    private var capturepDevice:AVCaptureDevice!
+
+    override init() {
+        super.init()
+        prepareCamera()
+        beginSession()
+    }
+
+    func takePhoto() {
+        _takePhoto = true
+    }
+
+    private func prepareCamera() {
+        captureSession.sessionPreset = .photo
+
+        if let availableDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices.first {
+            capturepDevice = availableDevice
+        }
+    }
+
+    private func beginSession() {
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: capturepDevice)
+
+            captureSession.addInput(captureDeviceInput)
+        } catch {
+            print(error.localizedDescription)
+        }
+
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.previewLayer = previewLayer
+
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String:kCVPixelFormatType_32BGRA]
+
+        if captureSession.canAddOutput(dataOutput) {
+            captureSession.addOutput(dataOutput)
+        }
+
+        captureSession.commitConfiguration()
+
+        let queue = DispatchQueue(label: "FromF.github.com.AVFoundationSwiftUI.AVFoundation")
+        dataOutput.setSampleBufferDelegate(self, queue: queue)
+    }
+
+    func startSession() {
+        if captureSession.isRunning { return }
+        captureSession.startRunning()
+    }
+
+    func endSession() {
+        if !captureSession.isRunning { return }
+        captureSession.stopRunning()
+    }
+
+    // MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        if _takePhoto {
+            _takePhoto = false
+            if let image = getImageFromSampleBuffer(buffer: sampleBuffer) {
+                DispatchQueue.main.async {
+                    // カメラロールに写真を保存
+                    self.image = image
+                }
+            }
+        }
+    }
+
+    @objc func didFinishSavingImage(_ image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutableRawPointer){
+        if error != nil {
+            print(error.code)
+        }
+    }
+
+    private func getImageFromSampleBuffer (buffer: CMSampleBuffer) -> UIImage? {
+        if let pixelBuffer = CMSampleBufferGetImageBuffer(buffer) {
+            let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+            let context = CIContext()
+
+            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
+
+            if let image = context.createCGImage(ciImage, from: imageRect) {
+                return UIImage(cgImage: image, scale: UIScreen.main.scale, orientation: .right)
+            }
+        }
+
+        return nil
+    }
+}
+// カメラを表示するビュー
+struct CALayerView: UIViewControllerRepresentable {
+    var caLayer:CALayer
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CALayerView>) -> UIViewController {
+        let viewController = UIViewController()
+        viewController.view.layer.addSublayer(caLayer)
+        caLayer.frame = viewController.view.layer.frame
+        return viewController
+    }
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<CALayerView>) {
+        caLayer.frame = uiViewController.view.layer.frame
+    }
+}
--- Sample9-Camera/ContentView.swift
+++ Sample9-Camera/ContentView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var camera = Camera()              // カメラ情報の初期設定
+    
     var body: some View {
+        CALayerView(caLayer: camera.previewLayer)          // カメラビューの表示
         Text("Hello, world!")
             .padding()
     }
