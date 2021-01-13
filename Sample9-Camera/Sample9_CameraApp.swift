5a63e62cd7592d47658813330af6ae95fa19cfc6
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-01-13 18:42:51 +0900

    Initial Commit

---
+++ Sample9-Camera/Sample9_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample9_CameraApp.swift
+//  Sample9-Camera
+//
+//  Created by keiji yamaki on 2021/01/13.
+//
+
+import SwiftUI
+
+@main
+struct Sample9_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
